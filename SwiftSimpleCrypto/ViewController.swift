f3ccedd2838755dc0745d27d89d016fa7d74b3c6
weixianlove <zhaoyongforever@qq..com>
2020-03-17 10:41:51 +0800

    Initial Commit

---
+++ SwiftSimpleCrypto/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftSimpleCrypto
+//
+//  Created by ZhaoYong on 2020/3/17.
+//  Copyright © 2020 ZhaoYong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
